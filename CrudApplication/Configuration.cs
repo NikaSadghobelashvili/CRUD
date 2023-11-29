6f9106c18e5e410d5256f8a43a7852792c30f734
Nika Sadgobelashvili <nika.sadgobelashvili09@gmail.com>
2023-11-29 20:31:56 +0400

    configurating dependency injection for unitOfwork

--- CrudApplication/Configuration.cs
+++ CrudApplication/Configuration.cs
@@ -10,8 +10,7 @@ namespace CrudApplication
     {
         public static void Configurate(IServiceCollection services, IConfiguration configuration)
         {
-            services.AddScoped<IRepository<User>,UserRepository>();
-            services.AddScoped<IRepository<UserProfile>, UserProfileRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddDbContext<CrudApplicationDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("CrudApplication")));
         }
