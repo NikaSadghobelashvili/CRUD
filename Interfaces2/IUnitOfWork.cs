c921e346fc0434b0e03b73d073a00e709363948e
Nika Sadgobelashvili <nika.sadgobelashvili09@gmail.com>
2023-11-29 20:33:29 +0400

    Re-Saving Changes

--- Interfaces2/IUnitOfWork.cs
+++ Interfaces2/IUnitOfWork.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-
+using DTO;
 
 namespace Interfaces
 {
     public interface IUnitOfWork : IDisposable
     {
-        
+        IRepository<User> UserRepository { get; }
+        IRepository<UserProfile> UserProfileRepository { get; }
         void Save();
     }
 }
--- Repository2/UnitOfWork.cs
+++ Repository2/UnitOfWork.cs
@@ -1,8 +1,29 @@
 ﻿using Interfaces;
+using DTO;
 namespace Repository
 {
-    public class UnitOfWork: IUnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly CrudApplicationDbContext _dbContext;
 
+        public UnitOfWork(CrudApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            UserRepository = new UserRepository(_dbContext);
+            UserProfileRepository = new UserProfileRepository(_dbContext);
+        }
+
+        public IRepository<User> UserRepository { get; }
+        public IRepository<UserProfile> UserProfileRepository { get; }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
     }
 }
